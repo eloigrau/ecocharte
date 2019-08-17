c1c1362759e0ac981320a167824cc34d014f7dc1
marchelibre <marchelibre@gmx.fr>
2019-08-18 01:08:58 +0200

    init

--- ecocharte/wsgi.py
+++ ecocharte/wsgi.py
@@ -7,6 +7,14 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
+
+import os
+
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
+from ecocharte import settings
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bourseLibre.settings")
 
 application = get_wsgi_application()
+application = WhiteNoise(application, root=settings.STATIC_ROOT)
+application.add_files(settings.STATIC_ADD_ROOT, prefix='more-files/')
