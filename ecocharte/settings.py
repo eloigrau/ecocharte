95d57f3eab190ec0101e95732a8f7615d7251299
marchelibre <marchelibre@gmx.fr>
2019-08-18 23:13:01 +0200

    init

--- ecocharte/settings.py
+++ ecocharte/settings.py
@@ -35,7 +35,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 LOCALL = False
 try:
     SECRET_KEY = os.environ['SECRET_KEY']
-    DEBUG = False
+    DEBUG = True
     ALLOWED_HOSTS = ['ecocharte.herokuapp.com']
 except:
     LOCALL  = True
