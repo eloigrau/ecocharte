fdaee10387b9dc10ebca3bedcdb1eba4844254c8
marchelibre <marchelibre@gmx.fr>
2019-08-26 11:50:30 +0200

    init

--- ecocharte/settings.py
+++ ecocharte/settings.py
@@ -211,8 +211,8 @@ BASE_URL = "https://ecocharte.herokuapp.com"
 ##    STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'static/'))
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/staticfiles/'
 
 PROJECT_ROOT2 = os.path.dirname(os.path.abspath(__file__))
 
